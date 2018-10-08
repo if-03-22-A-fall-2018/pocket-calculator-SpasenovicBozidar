172260edadf4f4863fffd58f06bb59f8202f120c
SpasenovicBozidar <spasenovic2002@gmail.com>
2018-10-08 15:49:32 +0200

    Working programm PocketCalculator

--- PocketCalculator.c
+++ PocketCalculator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int Add(float op1, float op2)
 {
   return op1 + op2;
@@ -21,61 +22,65 @@ float Divide(float op1, float op2)
 
 
 float GetOperand(){
-  float op = 0;
+  float op = 0.0;
   printf("Enter Operand: ");
   scanf("%f", &op);
   printf("\n");
   return op;
 }
 
-void Choise(op1, op2){
+void Choice(float op1,float op2,int choice){
+  float sum = 0.0;
 
-  if (choise == 1) {
+  if (choice == 1) {
       sum = Add(op1, op2);
       printf("%f + %f = %f\n", op1, op2,sum);
   }
-  else if (choise == 2) {
+  else if (choice == 2) {
       sum = Subtract(op1, op2);
       printf("%f - %f = %f\n", op1, op2,sum);
   }
-  else if (choise == 3) {
+  else if (choice == 3) {
       sum = Multiply(op1, op2);
       printf("%f * %f = %f\n", op1, op2,sum);
   }
-  else if (choise == 4) {
+  else if (choice == 4) {
       sum = Divide(op1, op2);
       printf("%f / %f = %f\n", op1, op2, sum);
   }
-  else if (choise == -1) {
-    printf("Programm stoped\n");
-  }
+
 }
 
 void Calculate() {
-  float sum = 0;
-  int choise = 0;
 
 
-  while (choise != -1) {
+  int choice = 0;
+
+  while (choice != -1) {
 
     printf("\n");
     printf("Choose one of the following operations: \n");
-    printf("Add(1) \nSubtract(2) \nMultiply(3) \nDivide(4) \nStop Programm(-1) \n");
+    printf("Add(1) \nSubtract(2) \nMultiply(3) \nDivide(4) \nStop Programm(-1)\n");
 
-    printf("Enter your choise :\n");
-    scanf("%d", &choise );
+    printf("Enter your choise : ");
+    scanf("%d", &choice );
+    if (choice == -1) {
+      printf("Programm stoped\n");
+
+      return;
+    }
     float op1= GetOperand();
     float op2= GetOperand();
+    Choice(op1, op2, choice);
 
-    Choise(op1, op2);
-
-
+}
 }
 
 int main(int argc, char const *argv[]) {
 
   printf("Pocket Calculator\n");
   Calculate();
+
 
   return 0;
 }
